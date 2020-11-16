4c7e891b8283ba03bb31d55d6a76d78cdc8715ee
Renan Perez <perez.vr@gmail.com>
2020-11-16 16:39:23 -0300

    Criando variáveis para conexão

--- CustomerWebApiDDD.Infrastruture.Repository/DBConnection/Connection.cs
+++ CustomerWebApiDDD.Infrastruture.Repository/DBConnection/Connection.cs
@@ -4,7 +4,9 @@ namespace CustomerWebApiDDD.Infrastruture.Repository.DBConnection
 {
     public class Connection
     {
-        private static string GetConnectionString() => $"Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=Customer;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+        private const string DBServer = "MSSQLLocalDB";
+        private const string DBCatalog = "Customer";
+        private static string GetConnectionString() => $"Data Source=(localdb)\\{DBServer};Initial Catalog={DBCatalog};Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
         internal static SqlConnection GetConnection() => new SqlConnection(GetConnectionString());
     }
 }
